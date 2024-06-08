f633ff5ab9041ed4dc953af22b42ec122a036557
blavikensbutcher <luckertheory@gmail.com>
2024-06-08 16:02:51 +0300

    styling

--- src/components/CampersList/CamperItem/CamperItem.jsx
+++ src/components/CampersList/CamperItem/CamperItem.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "../../../shared/components/Button/Button.jsx";
 import css from "./CamperItem.module.css";
 import SharedSVG from "../../../shared/sharedSVG/SharedSvg.jsx";
 import { truncateString } from "../../../../helpers/truncateString.js";
 import { TagsList } from "../../TagsList/TagsList.jsx";
-import {ModalWindow} from "../../../shared/components/Modal/ModalWindow.jsx";
+import { ModalWindow } from "../../../shared/components/Modal/ModalWindow.jsx";
 
 export const CamperItem = ({ data }) => {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
@@ -18,8 +17,8 @@ export const CamperItem = ({ data }) => {
   };
 
   const handleClick = () => {
-    showModal()
-  }
+    showModal();
+  };
 
   let avrgMark = 0; // Инициализируем переменную для хранения средней оценки
 
@@ -30,44 +29,53 @@ export const CamperItem = ({ data }) => {
   avrgMark /= data.reviews.length;
 
   return (
-      <>
-        <li className={css.card_item}>
-          <img src={data.gallery[0]} width={300} className={css.image} alt="Camper Image" />
-          <div className={css.global_container}>
-            <div className={css.description_container}>
-              <div className={css.header_name}>
-                <h2>{data.name}</h2>
-                <p className={css.price}>
-                  €{data.price}.00{" "}
-                  <button className={css.heart_btn}>
-                    <SharedSVG svgId={"heart"} className={css.heart_svg} />
-                  </button>
-                </p>
-              </div>
-              <div className={css.location_reviews_container}>
-              <span>
-                <SharedSVG
-                    svgId={"rating"}
-                    className={css.icon}
-                />{" "}
-                {avrgMark} ({data.reviews.length} reviews)
-              </span>
-                <span>
-                <SharedSVG svgId={"location"} className={css.location_svg} />{" "}
-                  {data.location}
-              </span>
-              </div>
-              <p className={css.camper_descr}>
-                {truncateString(data.description, 100)}
+    <>
+      <li className={css.card_item}>
+        <img
+          src={data.gallery[0]}
+          width={300}
+          className={css.image}
+          alt="Camper Image"
+        />
+        <div className={css.global_container}>
+          <div className={css.description_container}>
+            <div className={css.header_name}>
+              <h2>{data.name}</h2>
+              <p className={css.price}>
+                €{data.price}.00{" "}
+                <button className={css.heart_btn}>
+                  <SharedSVG svgId={"heart"} className={css.heart_svg} />
+                </button>
               </p>
             </div>
-            <div className={css.tags_container}>
-              <TagsList data={data} adults={data.adults} />
+            <div className={css.location_reviews_container}>
+              <span>
+                <SharedSVG svgId={"rating"} className={css.icon} /> {avrgMark} (
+                {data.reviews.length} reviews)
+              </span>
+              <span>
+                <SharedSVG svgId={"location"} className={css.location_svg} />{" "}
+                {data.location}
+              </span>
             </div>
-            <Button onClick={handleClick}>Show More</Button>
+            <p className={css.camper_descr}>
+              {truncateString(data.description, 100)}
+            </p>
           </div>
-        </li>
-        <ModalWindow data={data} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} showModal={showModal} handleCancel={handleCancel}/>
-      </>
+          <div className={css.tags_container}>
+            <TagsList data={data} adults={data.adults} />
+          </div>
+          <Button onClick={handleClick}>Show More</Button>
+        </div>
+      </li>
+      <ModalWindow
+        data={data}
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        showModal={showModal}
+        handleCancel={handleCancel}
+        avrgMark={avrgMark}
+      />
+    </>
   );
 };
--- src/shared/components/Modal/ModalWindow.jsx
+++ src/shared/components/Modal/ModalWindow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Modal } from "antd";
 import css from './ModalWindow.module.css'
 import SharedSVG from "../../sharedSVG/SharedSvg.jsx";
-export const ModalWindow = ({isModalOpen, showModal, handleCancel, setIsModalOpen, data}) => {
+export const ModalWindow = ({isModalOpen, showModal, handleCancel, avrgMark, data}) => {
 
     console.log(data)
 
@@ -12,20 +12,37 @@ export const ModalWindow = ({isModalOpen, showModal, handleCancel, setIsModalOpe
         open={isModalOpen}
         onCancel={handleCancel}
         footer={null}
-        width={902}
-        style={{padding: 40}}
+        width={982}
+        style={{ padding: 40 }}
       >
-          <div className={css.modal_container}>
-              <div className={css.description_container}>
-                  <div className={css.header_name}>
-                      <h2>{data.name}</h2>
-                      <p className={css.price}>
-                          €{data.price}.00{" "}
-                      </p>
-                  </div>
+        <div className={css.modal_container}>
+
+          <div className={css.description_container}>
+            <div>
+              <h2 className={css.header_name}>{data.name}</h2>
+                <div className={css.location_reviews_container}>
+                    {" "}
+                    <span>
+                  <SharedSVG svgId={"rating"} className={css.icon}/> {avrgMark}{" "}
+                        ({data.reviews.length} reviews)
+                </span>
+                    <span>
+                <SharedSVG svgId={"location"} className={css.location_svg}/>{" "}
+                        {data.location}
+              </span>
+                </div>
+                <p className={css.price}>€{data.price}.00 </p>
+            </div>
+
+              <div className={css.image_container}>
+                  <img className={css.image} src={data.gallery[0]} alt="Track image 1"/>
+                  <img className={css.image} src={data.gallery[1]} alt="Track image 2"/>
+                  <img className={css.image} src={data.gallery[2]} alt="Track image 3"/>
               </div>
+
           </div>
+        </div>
       </Modal>
     </>
-);
+  );
 };
